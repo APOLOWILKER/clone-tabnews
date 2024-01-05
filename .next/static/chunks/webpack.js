0ebf1f27742c509d6f9376ece513c3b568dc8a00
apolo wilker <wilkerapolocarvalho@gmail.com>
2024-01-04 22:30:16 -0300

    feat: edit text home

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b61deadc6b504fa4"; }
+/******/ 		__webpack_require__.h = function() { return "386910b8575575ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
